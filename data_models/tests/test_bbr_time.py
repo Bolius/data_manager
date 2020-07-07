de25fe28c46bb7d02409a7e271efe6a4aa3aa0e2
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2020-07-07 10:12:08 +0200

    Reduced calls to dawa in tests

--- data_models/tests/test_bbr_time.py
+++ data_models/tests/test_bbr_time.py
@@ -6,36 +6,43 @@ from .factories import add_houses
 
 
 class BBRTimeTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.addresses = add_houses(nr_houses=3)
+        cls.houses = [h.buldings.first() for h in cls.addresses]
+        cls.h1 = cls.houses[0]
+        cls.h2 = cls.houses[1]
+        cls.h3 = cls.houses[2]
+
+    @classmethod
+    def tearDownClass(cls):
+        [h.delete() for h in cls.houses]
+        [a.delete() for a in cls.addresses]
+
     def test_add_get_time_data(self):
-        houses = add_houses(nr_houses=3)
-        houses = [h.buldings.first() for h in houses]
-        [h1, h2, h3] = houses
-        h1.construction_year = 1990
-        h2.construction_year = 1992
-        h3.construction_year = 1992
-        h1.reconstruction_year = 1991
-        h2.reconstruction_year = None
-        h3.reconstruction_year = 1992
-        [h.save() for h in houses]
+        self.h1.construction_year = 1990
+        self.h2.construction_year = 1992
+        self.h3.construction_year = 1992
+        self.h1.reconstruction_year = 1991
+        self.h2.reconstruction_year = None
+        self.h3.reconstruction_year = 1992
+        [h.save() for h in self.houses]
         data = BBR.get_time_data()
         self.assertEqual([1990, 1991, 1992], data["time_range"])
         self.assertEqual([1, 1, 3], data["houses_per_year"])
         self.assertEqual([0, 1, 2], data["recon_per_year"])
 
     def test_get_rolling_avgs(self):
-        houses = add_houses(nr_houses=3)
-        houses = [h.buldings.first() for h in houses]
-        [h1, h2, h3] = houses
-        h1.construction_year = 1990
-        h1.building_area = 50
+        self.h1.construction_year = 1990
+        self.h1.building_area = 50
 
-        h2.construction_year = 1991
-        h2.building_area = 100
+        self.h2.construction_year = 1991
+        self.h2.building_area = 100
 
-        h3.construction_year = 1996
-        h3.building_area = 150
+        self.h3.construction_year = 1996
+        self.h3.building_area = 150
 
-        [h.save() for h in houses]
+        [h.save() for h in self.houses]
         data = BBR.get_rolling_avgs()
         self.assertEqual(list(range(1990, 1997)), data["time_range"])
         self.assertEqual(
@@ -43,19 +50,16 @@ class BBRTimeTest(TestCase):
         )
 
     def test_accumulated_sum_for_catatgorical(self):
-        houses = add_houses(nr_houses=3)
-        houses = [h.buldings.first() for h in houses]
-        [h1, h2, h3] = houses
-        h1.construction_year = 1990
-        h1.kitchen_facility = "E"
+        self.h1.construction_year = 1990
+        self.h1.kitchen_facility = "E"
 
-        h2.construction_year = 1991
-        h2.kitchen_facility = "E"
+        self.h2.construction_year = 1991
+        self.h2.kitchen_facility = "E"
 
-        h3.construction_year = 1992
-        h3.kitchen_facility = "F"
+        self.h3.construction_year = 1992
+        self.h3.kitchen_facility = "F"
 
-        [h.save() for h in houses]
+        [h.save() for h in self.houses]
         expected = [
             {"0": 0, "E": 1, "F": 0, "G": 0, "H": 0},
             {"0": 0, "E": 2, "F": 0, "G": 0, "H": 0},
