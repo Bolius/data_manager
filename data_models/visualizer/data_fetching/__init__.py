39b06a9869772254c83e561f1f978f83fe54a84e
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2020-09-02 13:13:48 +0200

    Updated tests

--- data_models/visualizer/data_fetching/__init__.py
+++ data_models/visualizer/data_fetching/__init__.py
@@ -1,17 +1,18 @@
 from .fetch_municipality_map import get_municipality_data
 from .fetch_time_data import (
     accumulated_sum_for_catatgorical,
-    get_rolling_avgs,
     get_time_data,
+    compute_time_data,
 )
 
 __all__ = [
     "accumulated_sum_for_catatgorical",
-    "get_rolling_avgs",
     "get_time_data",
     "get_municipality_data",
+    "compute_time_data",
 ]
 
 
 def fill_redis():
     get_municipality_data()
+    get_time_data()
