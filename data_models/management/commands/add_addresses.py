0cc2802ea9ce28c1b1230e9e737ea9fd76674207
Benjamin <benjamin@rotendahl.dk>
2020-09-10 10:34:53 +0200

    Docker build action (#88)
    * Reverted build
    
    * Ready to add data

--- data_models/management/commands/add_addresses.py
+++ data_models/management/commands/add_addresses.py
@@ -15,14 +15,14 @@ class Command(BaseCommand):
 
 
 def add_ids(file):
-    all_ids = set(House.objects.all().values_list("access_id", flat=True))
+    all_ids = set(House.objects.all().values_list("dawa_id", flat=True))
     with open(file, "r") as ids:
-        for line in tqdm(ids):
-            try:
-                if len(line) > 10:
-                    id = line[:-1]
-                    if id not in all_ids:
-                        House.add_house(access_id=id)
-            except Exception as e:
-                print(e)
-                continue
+        lines = [line[:-1] for line in ids.readlines() if len(line) > 10]
+        lines = [line for line in lines if line not in all_ids]
+    print(f"Already has {len(all_ids) - len(lines)} in data set")
+    for line in tqdm(lines):
+        try:
+            House.add_house(access_id=line)
+        except Exception as e:
+            print(e)
+            continue
