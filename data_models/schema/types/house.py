03ad60f88c51a866ca75c0f5772a8d0b3c33a2c2
Benjamin <benjamin@rotendahl.dk>
2020-11-11 10:10:27 +0100

    Deps update (#113)
    * Deps update
    
    * Updated example env
    
    * Created lock file

--- data_models/schema/types/house.py
+++ data_models/schema/types/house.py
@@ -3,6 +3,7 @@ import graphene
 from data_models.models import House as HOUSE_DB
 
 from .bbr_info import BBRInfo
+from .radon import kvhxToRadon
 
 
 class House(graphene.ObjectType):
@@ -10,12 +11,16 @@ class House(graphene.ObjectType):
     kvhxInput = graphene.String(required=False)
     # water_risk = graphene.NonNull(WaterRisk)
     bbr_info = graphene.NonNull(BBRInfo)
+    radon = graphene.Int(required=True)
 
     def resolve_bbr_info(parent, info):
         if parent.addressInput is not None:
             return HOUSE_DB.add_house(address_text=parent.addressInput).buldings.first()
         elif parent.kvhxInput is not None:
             return HOUSE_DB.add_house(kvhx=parent.kvhxInput).buldings.first()
+
+    def resolve_radon(parent, info):
+        return kvhxToRadon(parent.kvhxInput)
 
     #
     # def resolve_water_risk(parent, info):
--- data_models/schema/types/radon.py
+++ data_models/schema/types/radon.py
@@ -1,10 +1,76 @@
-import graphene
+import json
+import os
+import pickle
+
+import pandas as pd
+import requests
+
+with open("encoder.model", "rb") as e:
+    e = pickle.load(e)
+with open("scalar.model", "rb") as s:
+    s = pickle.load(s)
+with open("clf.model", "rb") as c:
+    c = pickle.load(c)
 
 
-class Radon(graphene.ObjectType):
-    class Meta:
-        name = "radon"
-        description = "Info om radon "
+def kvhxToRadon(kvhx):
+    data = kvhxToSeries(kvhx=kvhx)
+    encoded = encodeVals(data)
+    return predict(encoded)[0]
 
-    bqm3 = graphene.Float(required=True, description="Bq/m^3")
-    soil_type = graphene.String(required=True, description="Jordbundstype")
+
+def predict(ser):
+    ser.resityp_geo = e.transform([ser.resityp_geo])
+    ser = s.transform([ser])
+    return c.predict(ser)
+
+
+def kvhxToSeries(kvhx=None):
+    coords = "latlong"
+    url = "https://apps.conzoom.eu/api/v1"
+    print(f'Basic {os.environ["API_KEY"]}')
+    header = {"authorization": f'Basic {os.environ["API_KEY"]}'}
+    response = requests.request(
+        "GET",
+        f"{url}/match/dk/geocoder",
+        headers=header,
+        params={"in_kvhx": kvhx},
+    )
+    if response.status_code != 200:
+        raise ValueError("Non '200' return code from bbr")
+    data = json.loads(response.content)
+    if data["match"] is None or data["matchinfo"]["unadr_matchlvl"] in ["x", "i"]:
+        raise ValueError("No address found")
+    bld_data = requests.request(
+        "GET",
+        f"{url}/values/dk/unit/{data['match']['values']['unadr_primunit_key']}",
+        headers=header,
+        params={"coords": coords},
+    )
+    if bld_data.status_code != 200:
+        raise ValueError("Non '200' return code from bbr on bulding")
+    bld_data = json.loads(bld_data.content)
+    return bld_data["values"]
+
+
+def encodeVals(vals):
+    return pd.Series(
+        {
+            "zip": int(vals["pcode"]),
+            "has_basement": 0 if vals["bld_area_basement"] is None else 1,
+            "floors": vals["bld_floors"],
+            "kommune_id": int(vals["muni"]),
+            "build_year": int(vals["bld_conyear"].split("-")[0]),
+            "rebuild_year": (
+                int(vals["bld_reconyear"].split("-")[0])
+                if vals["bld_reconyear"] is not None
+                else 0
+            ),
+            "area_resi": vals["bld_area_resi"],
+            "area_basement": (
+                0 if vals["bld_area_basement"] is None else vals["bld_area_basement"]
+            ),
+            "resityp_geo": vals["unit_resityp_geo"],
+            "rooms": vals["unit_rooms"],
+        }
+    )
